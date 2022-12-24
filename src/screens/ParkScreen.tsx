16897e01f0404236806126fc1acd6196187ef584
Tom Hartley <tom@tomhrtly.com>
2022-12-24 14:28:51 +0000

    updates

--- src/screens/ParkScreen.tsx
+++ src/screens/ParkScreen.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ImageBackground, ScrollView, Text, View } from 'react-native';
 import getPark from '../api/endpoints/parks/getPark';
 import getTasks from '../api/endpoints/parks/getTasks';
 import Topbar from '../components/Topbar';
@@ -19,28 +19,40 @@ export default function ParkScreen({ route }) {
   return (
     <>
       <Topbar showBackButton={true} text={currentPark?.name} />
-      <ScrollView
+      <View
         style={{
           flex: 1,
           marginTop: -8,
-          paddingTop: 8,
         }}
       >
-        <Text>{currentPark?.completion_rate}% complete</Text>
-        <Text>
-          {tasks?.filter((task) => task.has_completed).length} out of{' '}
-          {tasks?.length} tasks unlocked.
-        </Text>
-        <Text>Park tasks:</Text>
-        {tasks?.map((task) => {
-          return (
-            <View key={task.id}>
-              <Text>Task name: {task.name}</Text>
-              <Text>Completed: {task.has_completed ? 'Yes' : 'No'}</Text>
-            </View>
-          );
-        })}
-      </ScrollView>
+        <ImageBackground
+          style={{
+            flex: 1,
+          }}
+          source={require('../../assets/images/screens/park/background.png')}
+        >
+          <ScrollView
+            style={{
+              paddingTop: 8,
+            }}
+          >
+            <Text>{currentPark?.completion_rate}% complete</Text>
+            <Text>
+              {tasks?.filter((task) => task.has_completed).length} out of{' '}
+              {tasks?.length} tasks unlocked.
+            </Text>
+            <Text>Park tasks:</Text>
+            {tasks?.map((task) => {
+              return (
+                <View key={task.id}>
+                  <Text>Task name: {task.name}</Text>
+                  <Text>Completed: {task.has_completed ? 'Yes' : 'No'}</Text>
+                </View>
+              );
+            })}
+          </ScrollView>
+        </ImageBackground>
+      </View>
     </>
   );
 }
