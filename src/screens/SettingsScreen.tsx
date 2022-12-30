5cb7f1af3c49c9539e116989f7ca11b906fb4b0a
Tom Hartley <tom@tomhrtly.com>
2022-12-30 14:58:36 +0000

    updates

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,5 +1,5 @@
 import Topbar from '../components/Topbar';
-import { Alert, PlatformColor, SafeAreaView } from 'react-native';
+import { Alert, PlatformColor, SafeAreaView, ScrollView } from 'react-native';
 import { AuthContext } from '../context/AuthProvider';
 import { useCallback, useContext } from 'react';
 import { Cell, Section, TableView } from 'react-native-tableview-simple';
@@ -22,89 +22,91 @@ export default function SettingsScreen() {
   return (
     <>
       <Topbar text="Settings" showBackButton={true} />
-      <SafeAreaView>
-        <TableView>
-          <Section header={'General'.toUpperCase()}>
-            <Cell
-              title="Username"
-              cellStyle="RightDetail"
-              detail={user.screen_name}
-            />
-            <Cell
-              title="Theme Park Shark ID"
-              cellStyle="RightDetail"
-              detail={user.email}
-            />
-            <Cell
-              title="Joined on"
-              cellStyle="RightDetail"
-              detail={dayjs(user.created_at).format('DD MMMM YYYY')}
-            />
-          </Section>
-          <Section header={'Help'.toUpperCase()}>
-            <Cell
-              title="Terms of Service"
-              titleTextStyle={{
-                color: PlatformColor('systemBlue'),
-              }}
-              onPress={() =>
-                WebBrowser.openBrowserAsync(
-                  'https://themeparkshark.com/info/terms-of-service'
-                )
-              }
-            />
-            <Cell
-              title="Privacy Policy"
-              titleTextStyle={{
-                color: PlatformColor('systemBlue'),
-              }}
-              onPress={() =>
-                WebBrowser.openBrowserAsync(
-                  'https://themeparkshark.com/info/privacy-policy'
-                )
-              }
-            />
-          </Section>
-          <Section
-            footer={`© Theme Park Shark ${new Date().getFullYear()} v1.0`}
-          >
-            <Cell
-              title="Deactivate My Account"
-              titleTextStyle={{
-                color: PlatformColor('systemRed'),
-              }}
-              onPress={() => {
-                Alert.alert(
-                  'Are you sure you want to delete your account?',
-                  'You cannot undo this action.',
-                  [
-                    {
-                      text: 'Cancel',
-                      style: 'cancel',
-                    },
-                    {
-                      text: 'Delete',
-                      style: 'destructive',
-                      onPress: async () => {
-                        await deleteUser();
-                        await logout();
-
-                        RootNavigation.navigate('Login');
+      <SafeAreaView style={{marginTop: -8, flex: 1,}}>
+        <ScrollView>
+          <TableView>
+            <Section header={'General'.toUpperCase()}>
+              <Cell
+                title="Username"
+                cellStyle="RightDetail"
+                detail={user.screen_name}
+              />
+              <Cell
+                title="Theme Park Shark ID"
+                cellStyle="RightDetail"
+                detail={user.email}
+              />
+              <Cell
+                title="Joined on"
+                cellStyle="RightDetail"
+                detail={dayjs(user.created_at).format('DD MMMM YYYY')}
+              />
+            </Section>
+            <Section header={'Help'.toUpperCase()}>
+              <Cell
+                title="Terms of Service"
+                titleTextStyle={{
+                  color: PlatformColor('systemBlue'),
+                }}
+                onPress={() =>
+                  WebBrowser.openBrowserAsync(
+                    'https://themeparkshark.com/info/terms-of-service'
+                  )
+                }
+              />
+              <Cell
+                title="Privacy Policy"
+                titleTextStyle={{
+                  color: PlatformColor('systemBlue'),
+                }}
+                onPress={() =>
+                  WebBrowser.openBrowserAsync(
+                    'https://themeparkshark.com/info/privacy-policy'
+                  )
+                }
+              />
+            </Section>
+            <Section
+              footer={`© Theme Park Shark ${new Date().getFullYear()} v1.0`}
+            >
+              <Cell
+                title="Deactivate My Account"
+                titleTextStyle={{
+                  color: PlatformColor('systemRed'),
+                }}
+                onPress={() => {
+                  Alert.alert(
+                    'Are you sure you want to delete your account?',
+                    'You cannot undo this action.',
+                    [
+                      {
+                        text: 'Cancel',
+                        style: 'cancel',
                       },
-                    },
-                  ]
-                );
-              }}
-            />
-            <Cell
-              title="Sign Out"
-              titleTextStyle={{
-                color: PlatformColor('systemBlue'),
-              }}
-              onPress={() => logout()}
-            />
-          </Section>
-        </TableView>
+                      {
+                        text: 'Delete',
+                        style: 'destructive',
+                        onPress: async () => {
+                          await deleteUser();
+                          await logout();
+
+                          RootNavigation.navigate('Login');
+                        },
+                      },
+                    ]
+                  );
+                }}
+              />
+              <Cell
+                title="Sign Out"
+                titleTextStyle={{
+                  color: PlatformColor('systemBlue'),
+                }}
+                onPress={() => logout()}
+              />
+            </Section>
+          </TableView>
+        </ScrollView>
       </SafeAreaView>
     </>
   );
