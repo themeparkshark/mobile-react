68d91941982442313f6fd211dd6e811802e433a5
Tom Hartley <tom@tomhrtly.com>
2023-08-25 22:46:17 +0100

    updates

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 export default {
   name: 'Theme Park Shark',
   slug: 'mobile-react',
-  version: '1.0.15',
+  version: '1.0.16',
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'light',
