f52de83eef0a96b93087a2ab72255ff70fb6940a
Tom Hartley <tom@tomhrtly.com>
2023-10-06 18:11:09 +0100

    Update timeout

--- src/context/LocationProvider.tsx
+++ src/context/LocationProvider.tsx
@@ -1,17 +1,22 @@
-import { createContext, Dispatch, FC, ReactNode, useState } from 'react';
-import { useIntervalWhen } from 'rooks';
+import {
+  createContext,
+  FC,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 import checkForPark from '../helpers/check-for-park';
 import getCurrentLocation from '../helpers/get-current-location';
 import { LocationType } from '../models/location-type';
 import { ParkType } from '../models/park-type';
+import { AuthContext } from './AuthProvider';
 
 export interface LocationContextType {
   readonly location?: LocationType;
-  readonly setLocation: Dispatch<any>;
   readonly requestLocation: () => void;
   readonly requestPark: () => void;
   readonly park?: ParkType;
-  readonly parkLoaded: boolean;
 }
 
 export const LocationContext = createContext<LocationContextType>(
@@ -19,10 +24,10 @@ export const LocationContext = createContext<LocationContextType>(
 );
 
 export const LocationProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const [location, setLocation] = useState<LocationType>();
+  const [location, setLocation] = useState<LocationType | undefined>();
   const [park, setPark] = useState<ParkType>();
-  const [parkLoaded, setParkLoaded] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
+  const { user } = useContext(AuthContext);
 
   const requestLocation = async () => {
     setLocation(await getCurrentLocation());
@@ -31,12 +36,15 @@ export const LocationProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const requestPark = async () => {
     setLoading(true);
     setPark(await checkForPark());
-    setParkLoaded(true);
     setLoading(false);
   };
 
-  useIntervalWhen(
-    async () => {
+  useEffect(() => {
+    if (!user) {
+      return;
+    }
+
+    const interval = setInterval(async () => {
       if (loading) {
         return;
       }
@@ -45,20 +53,18 @@ export const LocationProvider: FC<{ children: ReactNode }> = ({ children }) => {
       await requestLocation();
       await requestPark();
       setLoading(false);
-    },
-    5000,
-    Boolean(location)
-  );
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [user?.id]);
 
   return (
     <LocationContext.Provider
       value={{
         location,
-        setLocation,
         requestLocation,
         requestPark,
         park,
-        parkLoaded,
       }}
     >
       {children}
