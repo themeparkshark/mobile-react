1eb2b05d9f1d41c595bf58723257d9d05b7fb090
Tom Hartley <tom@tomhrtly.com>
2023-10-28 12:12:46 +0100

    update build number

--- app.config.js
+++ app.config.js
@@ -22,7 +22,7 @@ export default {
   },
   assetBundlePatterns: ['**/*'],
   ios: {
-    buildNumber: '20231028.1',
+    buildNumber: '20231028.2',
     bitcode: 'Debug',
     usesAppleSignIn: true,
     bundleIdentifier: 'com.tomhartley97.mobile-react',
