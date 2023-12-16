f3ab720695ed22ff2fa2a6b6f814731cc1926e6a
Tom Hartley <tom@tomhrtly.com>
2023-12-16 16:06:23 +0000

    updates

--- src/api/endpoints/players/unfriend.ts
+++ src/api/endpoints/players/unfriend.ts
@@ -2,5 +2,5 @@ import { PlayerType } from '../../../../models/player-type';
 import client from '../../../client';
 
 export default async function unfriend(player: PlayerType) {
-  await client.delete(`/me/players/${player.id}/unfriend`);
+  await client.delete(`/players/${player.id}/unfriend`);
 }
