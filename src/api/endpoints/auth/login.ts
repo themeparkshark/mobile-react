785209b6eed8cdbe6eddc267e7967001a1a4e2c4
Tom Hartley <tom@tomhrtly.com>
2023-12-16 14:35:57 +0000

    updates

--- src/api/endpoints/auth/login.ts
+++ src/api/endpoints/auth/login.ts
@@ -2,14 +2,12 @@ import { ApiResponseType } from '../../../models/api-response-type';
 import { PlayerType } from '../../../models/player-type';
 import client from '../../client';
 
-export default async function login(credential: {
-  readonly user: string;
-  readonly identity_token: string;
-}): Promise<PlayerType> {
+export default async function login(user: string, identity_token: string): Promise<PlayerType> {
   const response = await client.post<ApiResponseType<PlayerType>>(
     '/auth/login',
     {
-      credential,
+      user,
+      identity_token,
     }
   );
 
