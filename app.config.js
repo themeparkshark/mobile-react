d4b4c98ef4e15cc7db11e0435dc5876b37133d61
Dustin Sparage <dustinsparage@Mac-Studio.local>
2026-02-05 20:55:43 -0800

    Bump build number to 20260206.1 for TestFlight resubmit

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
   },
   assetBundlePatterns: ['**/*'],
   ios: {
-    buildNumber: '20260205.1',
+    buildNumber: '20260206.1',
     bitcode: 'Debug',
     usesAppleSignIn: true,
     bundleIdentifier: 'com.themeparkshark.app',
